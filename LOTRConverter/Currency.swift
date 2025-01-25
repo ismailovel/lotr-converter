b78dd9ffe3ea5904a0181d1c3b9dddb035c98898
Elchin Ismailov <ismailowwork@gmail.com>
2025-01-25 22:07:27 +0400

    Add Currency model

---
+++ LOTRConverter/Currency.swift
@@ -0,0 +1,14 @@
+//
+//  Currency.swift
+//  LOTRConverter
+//
+//  Created by Elchin Ismailov on 25.01.2025.
+//
+
+enum Currency: Double {
+    case copperPenny = 6400
+    case silverPenny = 64
+    case silverPiece = 16
+    case goldPenny = 4
+    case goldPiece = 1
+}
